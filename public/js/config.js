c460bf336d170e546a6c4609b029f6bedab0e666
RiyanBK <rkadribegovic1@hwemail.com>
2025-03-20 04:20:50 -0700

    config.js

---
+++ public/js/config.js
@@ -0,0 +1,8 @@
+// public/js/config.js
+const firebaseConfig = {
+    CONTRACT_ADDRESS: "0xa7B5E39CA6C545C3151118719910ee6881FFAA56",
+    NETWORK_ID: 11155111  // Sepolia
+  };
+  
+  // In contract.js, replace hardcoded values with these
+  const CONTRACT_ADDRESS = firebaseConfig.CONTRACT_ADDRESS;
